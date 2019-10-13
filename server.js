03745adbf35b20b376f22be2c08905ea5b8e456d
TomasS <43637908+GrimReaper6667@users.noreply.github.com>
2019-10-13 22:34:53 +0200

    live

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ const loginValidation = require('./routes/login-validation.js')
 const registerValidation = require('./routes/reg-validation.js');
 const matches = require('./routes/matches.js');
 
-// const url = process.env.MONGODB_URI;
-const url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
+const url = process.env.MONGODB_URI;
+// const url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT + '/' + process.env.DB_NAME;
 
 mongoose.connect(url, {
   useNewUrlParser: true
